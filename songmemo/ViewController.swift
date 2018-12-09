153bab2ed2ebbc81fd76767589dd859e2a038bfc
Tristan <tristancalderbank@gmail.com>
2018-12-09 14:25:02 -0800

    Initial Commit

---
+++ songmemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  songmemo
+//
+//  Created by testuser1 on 2018-12-09.
+//  Copyright © 2018 songmemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
