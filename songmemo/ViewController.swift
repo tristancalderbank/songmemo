8c31e1bf454c6ef6b68ffd627e42f88eaef9aac4
Tristan <tristancalderbank@gmail.com>
2018-12-10 23:02:49 -0800

    initial app

--- songmemo/ViewController.swift
+++ songmemo/ViewController.swift
@@ -7,14 +7,108 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDelegate, UITableViewDataSource {
 
+    var recordingSession:AVAudioSession!
+    var audioRecorder:AVAudioRecorder!
+    var audioPlayer:AVAudioPlayer!
+    
+    var numberOfRecords:Int = 0
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    @IBOutlet weak var recordingsTableView: UITableView!
+    
+    @IBAction func record(_ sender: Any) {
+        // Check if we have an active recorder
+        if audioRecorder == nil {
+            numberOfRecords += 1
+            
+            let filename = getDirectory().appendingPathComponent("\(numberOfRecords).m4a")
+            
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            // Start audio recording
+            do {
+                audioRecorder = try AVAudioRecorder(url: filename, settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.record()
+                
+                buttonLabel.setTitle("Stop Recording", for: .normal)
+            }
+            catch {
+                displayAlert(title: "Oops!", message: "Recording audio failed.")
+            }
+            
+        }
+        else {
+            // Stop audio recording
+            audioRecorder.stop()
+            audioRecorder = nil
+            
+            UserDefaults.standard.set(numberOfRecords, forKey: "numberOfRecords")
+            recordingsTableView.reloadData()
+            
+            buttonLabel.setTitle("Start Recording", for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setting up session
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        if let number:Int = UserDefaults.standard.object(forKey: "numberOfRecords") as? Int {
+            numberOfRecords = number
+        }
+        
+        AVAudioSession.sharedInstance().requestRecordPermission { (hasPermission) in
+            if hasPermission {
+                print ("Accepted")
+            }
+        }
+    }
+    
+    func getDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = paths[0]
+        return documentDirectory
+    }
+    
+    func displayAlert(title:String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "dismiss", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRecords
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row + 1)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let path = getDirectory().appendingPathComponent("\(indexPath.row + 1).m4a")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: path)
+            audioPlayer.play()
+        }
+        catch {
+            displayAlert(title: "Oops!", message: "Failed to play audio.")
+        }
+    }
 }
 
